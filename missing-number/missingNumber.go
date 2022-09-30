1d1dc083e18add7e9f31c1ce9a63d776ce2157ad
Alexander Rosenthal <alexrosenthalster@gmail.com>
2022-09-30 16:18:26 -0400

    missing number cycle sort go

--- missing-number/missingNumber.go
+++ missing-number/missingNumber.go
@@ -5,20 +5,27 @@ import "fmt"
 // Given a range from [0, n], return the number missing in the range
 func missingNumber(nums []int) int {
 	n := len(nums)
-	sum := 0
-	missingSum := 0
-	for i := 1; i <= n; i++ {
-		sum += i
-	}
+	//sum := 0
+	//missingSum := 0
+	//for i := 1; i <= n; i++ {
+	//	sum += i
+	//}
 	i := 0
 	// cycle sort
 	for i < n {
 		v := nums[i]
-		missingSum += v
+		//missingSum += v
+		if (i != v && v != n) {
+			nums[i], nums[v] = nums[v], nums[i]
+		}
 		i++
 	}
-	return sum - missingSum
-
+	for i, v := range(nums) {
+		if (i != v) {
+			return i
+		}
+	}
+	return -1
 }
 
 func main() {
