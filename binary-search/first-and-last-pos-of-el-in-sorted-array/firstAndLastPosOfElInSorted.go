b60b2545c9c3a4c0ed4a139f3d5f5465a4937135
Alexander Rosenthal <alexrosenthalster@gmail.com>
2022-10-26 17:04:06 -0400

    optimized with binary search

--- binary-search/first-and-last-pos-of-el-in-sorted-array/firstAndLastPosOfElInSorted.go
+++ binary-search/first-and-last-pos-of-el-in-sorted-array/firstAndLastPosOfElInSorted.go
@@ -5,31 +5,36 @@ import "fmt"
 func getIndices(nums []int, target int) []int {
 	// given an array sorted in ascending order
 	// return the positions of target integer
+	result := []int{-1, -1}
+	
+	result[0] = search(nums, target, true)
+	result[1] = search(nums, target, false)
+
+	return result
+}
+
+func search(nums []int, target int, firstIndex bool) int {
 	s := 0
 	e := len(nums) - 1
-	
-	result := []int{-1, -1}
+	a := -1
 
 	for s <= e {
-
-		// find first occurrence of target
 		mid := s + (e - s) / 2
-		
-		if nums[mid] > target {
-			e = mid - 1
-		} else if nums[mid] < target {
-			s = mid + 1
-		} else if nums[mid + 1] != target {
-			result[0] = mid
 
-			// find second occurrence of target
-			e = mid - 1
-		} else if nums[mid - 1] != target {
-			result[1] = mid
+		if target > nums[mid] {
 			s = mid + 1
+		} else if target < nums[mid] {
+			e = mid - 1
+		} else {
+			a = mid
+			if (firstIndex) {
+				e = mid - 1
+			} else {
+				s = mid + 1
+			}
 		}
 	}
-	return result
+	return a
 }
 
 func main() {
