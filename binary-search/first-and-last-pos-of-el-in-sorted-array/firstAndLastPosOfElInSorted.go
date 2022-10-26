5afd5ac2817d1f2991df3ac2a7fecc0aadad3532
Alexander Rosenthal <alexrosenthalster@gmail.com>
2022-10-26 16:47:47 -0400

    first and last positions of target element in sorted array go

--- binary-search/first-and-last-pos-of-el-in-sorted-array/firstAndLastPosOfElInSorted.go
+++ binary-search/first-and-last-pos-of-el-in-sorted-array/firstAndLastPosOfElInSorted.go
@@ -8,28 +8,25 @@ func getIndices(nums []int, target int) []int {
 	s := 0
 	e := len(nums) - 1
 	
-	result := []int{}
+	result := []int{-1, -1}
 
-	for s < e {
-		mid := s + (e -s) / 2
+	for s <= e {
 
-		if target > nums[mid] {
-			s = mid + 1
-		} else if target < nums[mid] {
+		// find first occurrence of target
+		mid := s + (e - s) / 2
+		
+		if nums[mid] > target {
 			e = mid - 1
-		} else {
-			above := mid + 1
-			below := mid - 1
-			result = append(result, mid)
-			for target == nums[above] {
-				result = append(result, above)
-				above++
-			}
-			for target == nums[below] {
-				result = append([]int{below}, result...)
-				below--
-			}
-			return result
+		} else if nums[mid] < target {
+			s = mid + 1
+		} else if nums[mid + 1] != target {
+			result[0] = mid
+
+			// find second occurrence of target
+			e = mid - 1
+		} else if nums[mid - 1] != target {
+			result[1] = mid
+			s = mid + 1
 		}
 	}
 	return result
